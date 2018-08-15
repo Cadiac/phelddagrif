19fdeb1b00b3e7ea9b90debbccee9766fb33f32c
Jaakko Husso <jaakko.husso@futurice.com>
2018-08-16 02:51:14 +0300

    Create database and set it up with Terraform

--- terraform/phelddagrif-dev.tf
+++ terraform/phelddagrif-dev.tf
@@ -17,9 +17,13 @@ resource "digitalocean_droplet" "phelddagrif-dev" {
 	provisioner "remote-exec" {
     inline = [
       "export PATH=$PATH:/usr/bin",
+      # Add repositories
       "wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb",
+      "sudo add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main'",
+      "wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -",
       "sudo dpkg -i erlang-solutions_1.0_all.deb",
       "sudo apt-get update",
+      # "sudo apt-get -y upgrade",
       "sudo apt-get -y install build-essential",
       "sudo apt-get -y install git",
       # Install nginx
@@ -28,12 +32,25 @@ resource "digitalocean_droplet" "phelddagrif-dev" {
       "sudo apt-get -y install esl-erlang",
       "sudo apt-get -y install elixir",
       # Add user for deployments and set its password
-      "sudo adduser --disabled-password --gecos \"\" deploy",
-      "echo \"deploy:${var.deploy_password}\"|chpasswd",
+      "sudo adduser --disabled-password --gecos '' deploy",
+      "echo 'deploy:${var.deploy_password}'|chpasswd",
       "sudo usermod -aG sudo deploy",
       "sudo mkdir /home/deploy/.ssh",
       "sudo cp /root/.ssh/authorized_keys /home/deploy/.ssh/authorized_keys",
       "sudo chown deploy /home/deploy/.ssh/authorized_keys",
+      # Setup firewall
+      "sudo ufw allow OpenSSH",
+      "sudo ufw --force enable",
+      # Install postgres
+      "sudo apt-get -y install postgresql-10",
+      # Create database
+      "sudo -u postgres psql -c \"CREATE DATABASE phelddagrif_dev;\"",
+      "sudo -u postgres psql -c \"CREATE USER phelddagrif WITH PASSWORD '${var.db_password}';\"",
+      "sudo -u postgres psql -c \"GRANT ALL PRIVILEGES ON DATABASE phelddagrif_dev TO phelddagrif;\"",
+      # Install nodejs
+      "curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh",
+      "sudo bash nodesource_setup.sh",
+      "sudo apt-get -y install nodejs",
       # Disable root SSH login
       "sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config",
       "sudo service sshd restart"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,6 +3,7 @@ variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 variable "deploy_password" {}
+variable "db_password" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
