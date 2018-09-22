7a471a2c11d0f64e5b2c5d0982169f7c65bf0bcd
Jaakko Husso <jaakko.husso@futurice.com>
2018-09-23 00:01:25 +0300

    Bulk card import

--- lib/tasks/import_data.ex
+++ lib/tasks/import_data.ex
@@ -15,7 +15,8 @@ defmodule Mix.Tasks.Data.Import do
     Logger.info("Importing sets")
     fetch_sets()
     Logger.info("Importing cards")
-    fetch_pages(true, "https://api.scryfall.com/cards")
+    # fetch_pages(true, "https://api.scryfall.com/cards")
+    fetch_cards_bulk()
     Logger.info("Import done!")
   end
 
@@ -50,8 +51,9 @@ defmodule Mix.Tasks.Data.Import do
     end
   end
 
-  defp fetch_pages(has_more_pages, _url) when has_more_pages === false, do: Logger.info("Found the last page.")
-  defp fetch_pages(has_more_pages, url) do
+  defp fetch_cards_bulk() do
+    url = "https://archive.scryfall.com/json/scryfall-oracle-cards.json"
+
     Logger.info("GET #{url}")
 
     options = [recv_timeout: 60_000]
@@ -60,13 +62,8 @@ defmodule Mix.Tasks.Data.Import do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         response = body |> Poison.decode!
 
-        has_more_pages = Map.get(response, "has_more")
-        next_page_url = Map.get(response, "next_page")
-
         # Insert cards to database
         response
-        |> Map.get("data")
-        |> Enum.filter(fn(card) -> Map.get(card, "lang") == "en" end)
         |> Enum.map(fn(card) ->
           card_with_scryfall_id = Map.put_new(card, "scryfall_id", Map.get(card, "id"))
 
@@ -78,25 +75,9 @@ defmodule Mix.Tasks.Data.Import do
           end
         end)
 
-        Logger.info("Processed #{response |> Map.get("data") |> length} cards")
-
-        fetch_pages(has_more_pages, next_page_url)
-      {:ok, %HTTPoison.Response{status_code: 429}} ->
-        Logger.error("Hit rate limit, sleeping for a minute")
-        Process.sleep(60_000)
-        fetch_pages(has_more_pages, url)
-      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
-        Logger.error("Got unknown status_code #{status_code}: #{inspect body}")
-        Process.sleep(60_000)
-        fetch_pages(has_more_pages, url)
+        Logger.info("Processed #{response |> length} cards")
       {:error, %HTTPoison.Error{reason: reason}} ->
-        Logger.error("API error while fetching cards: #{inspect reason}")
-        Process.sleep(5000)
-        fetch_pages(has_more_pages, url)
-      {:error, err} ->
-        Logger.error("Unknown error #{inspect err}")
-        Process.sleep(5000)
-        fetch_pages(has_more_pages, url)
+        Logger.error("API error while fetching bulk cards: #{inspect reason}")
     end
   end
 end
